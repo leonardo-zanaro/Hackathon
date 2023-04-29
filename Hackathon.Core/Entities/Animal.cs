7802a17a5ce5e7d14982093b93f83e9b87c3eb66
Leonardo Zanaro <leonardo.zanaro@gamasti.com.br>
2023-04-29 13:54:51 -0300

    Adicionar arquivos de projeto.

---
+++ Hackathon.Core/Entities/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Core.Entities
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public string MicroChip { get; set; }
+        public DateTime? Nascimento { get; set; }
+
+        [ForeignKey("TipoAnimal")]
+        public Guid TipoAnimalId { get; set; }
+    }
+}
